1d75c9d5cb3ee4cd51b4aab191fd7c6f2d2255fe
pdisi <pdisidoro@yahoo.com>
2021-10-03 20:43:01 -1000

    assessment complete

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -1,5 +1,7 @@
 // ASSESSMENT 4: JavaScript Coding Practical Questions with Jest
 
+
+
 // Please read all questions thoroughly
 // Pseudo coding is REQUIRED
 // If you get stuck, please leave comments to help us understand your thought process
@@ -14,15 +16,42 @@
 // --------------------1) Create a function that takes in an array, removes the first item from the array and shuffles the remaining content.
 
 // a) Create a test with an expect statement using the variable provided. HINT: Check out this resource: https://jestjs.io/docs/expect#expectarraycontainingarray
+// a describe method that lists the name of the function OR naming of the particular test.
+
+describe("Remove and shuffle", () => {
+
+    // a test/it method, nested within the describe block, that in plain words, describes what the function does.
+    it("Takes in an array then removes the first item and also shuffles the remaining content", () => {
+
+      //an expect method, nested within the test block, calling on the hello() function, followed by the .toEqual() matcher that checks the expected output of the function return.
+      expect(input(colors1)).toEqual((["yellow", "blue", "pink", "green"]))
+      expect(input(colors2)).toEqual((["saffron", "aquamarine", "periwinkle", "indigo", "ochre"]))
+    })
+})
+
+//Test Fail: Input is not defined
 
 var colors1 = ["purple", "blue", "green", "yellow", "pink"]
 // Expected output example (can be a different order): ["yellow", "blue", "pink", "green"]
 var colors2 = ["chartreuse", "indigo", "periwinkle", "ochre", "aquamarine", "saffron"]
 // Expected output example (can be a different order): ["saffron", "aquamarine", "periwinkle", "indigo", "ochre"]
 
-
-
 // b) Create the function that makes the test pass.
+
+//Make an generic arrow function called input
+//Use the shift method to remove the first item
+//Use a let statement that is equal to a sort method that use a math.random
+//Return the let
+const input = (array) => {
+    array.shift
+    let newArray = array.sort(() => Math.random() - 0.5);{
+        return newArray
+    }     
+}
+
+//Test Fail: > 27 |       expect(input(colors1)).toEqual((["yellow", "blue", "pink", "green"]))
+
+//Notes on process: This took a bit of time for me to research and write out properly, but the syntax is beginning to bit easier for me to grasp. It appears that my function is working properly. I'm not sure why the test is failing for colors1
 
 
 
@@ -30,20 +59,57 @@ var colors2 = ["chartreuse", "indigo", "periwinkle", "ochre", "aquamarine", "saf
 
 // a) Create a test with expect statements for each of the variables provided.
 
-var nums1 = [3, 56, 90, -8, 0, 23, 6]
+ describe("Numbers Min to Max", () => {
+
+//     // a test/it method, nested within the describe block, that in plain words, describes that the function does.
+    it("The function takes in an array of numbers then returns only the least and greatest numbers in numerical order", () => {
+
+//       //an expect method, nested within the test block, calling on the hello() function, followed by the .toEqual() matcher that checks the expected output of the function return.
+       expect(numberz([3, 56, 90, -8, 0, 23, 6])).toEqual(([-8, 90]))
+       expect(numberz([109, 5, 9, 67, 8, 24])).toEqual(([5, 109]))
+//     })
+// })
+
+ var nums1 = [3, 56, 90, -8, 0, 23, 6]
 // Expected output: [-8, 90]
 var nums2 = [109, 5, 9, 67 8, 24]
 // Expected output: [5, 109]
 
 
-
 // b) Create the function that makes the test pass.
+// Create a function called numberz to match the jest test
+// It takes in an array
+// I would then like to sort the numbers from least to greatest
+// Then take the first and last items in the array to return the least and greatest numbers
+
+// const numberz = (array) => {
+//     array.sort((a,b)=>a-b)
+//     let small = array[0]
+//     let big = array[array.length-1]{
+//         return small + big
+//     }
+// }
+
+//Notes on process: This test is saying "jest encountered an unexpected token". Not sure what is causing this.
+// The test for the first problem ran just fine and I'm not noticing any differences. I believe that the logic for my function is proper and should work well though.
+//It was exciting to come up with this logical process on my own.
 
 
 
 // --------------------3) Create a function that takes in two arrays as arguments and returns one array with no duplicate values. STRETCH: Use the spread operator to pass in a dynamic number of arguments.
 
 // a) Create a test with an expect statement using the variables provided.
+
+ describe("Dual array", () => {
+
+//     // a test/it method, nested within the describe block, that in plain words, describes that the function does.
+     it("Takes in to arrays and gives back one unique array with no duplicates", () => {
+
+//       //an expect method, nested within the test block, calling on the hello() function, followed by the .toEqual() matcher that checks the expected output of the function return.
+       expect(arrayz([3, 7, 10, 5, 4, 3, 3],[7, 8, 2, 3, 1, 5, 4])).toEqual(([3, 7, 10, 5, 4, 8, 2, 1]))
+//       expect(input()).toEqual(())
+//     })
+// })
 
 var testArray1 = [3, 7, 10, 5, 4, 3, 3]
 var testArray2 = [7, 8, 2, 3, 1, 5, 4]
@@ -52,3 +118,27 @@ var testArray2 = [7, 8, 2, 3, 1, 5, 4]
 
 
 // b) Create the function that makes the test pass.
+
+//create a function called arrayz
+//take in two arrays
+//combine the two arrays
+//iterate through it to find duplicates
+//return one array with no duplicates
+
+const arrayz = (array) => {
+    array.concat
+    let newarrayz = array.concat
+    for (let i = 0; i < array.length; i++){
+        if (map[array[i]]){
+            continue;
+        } else {
+            result.push(array[i])
+            map[array[i]] = true
+        }
+    }
+    return newarrayz
+}
+
+//Notes on process: Jest is encountering an error again and now it's pointing at this line at 142 for my notes on process. Never had these issues before.
+//Verified that I deleted the code that VS code keeps putting in at the top of the file. My logic seems to be straight forward for this function and I believe it should work.
+//Starting to have more fun with Javascript! Big win for the weekend.
--- interview-questions.md
+++ interview-questions.md
@@ -7,52 +7,52 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. What are props in React?
 
-  Your answer:
+  Your answer: Props are essentially vessels for passing information from a parent file to a child file. Data is exchanged with props in order to properly execute functions.
 
-  Researched answer:
+  Researched answer: They are used for passing data from one component to another. The data is only passed from the parent to the child, which is called uni-directional flow.
 
 
 
 2. What is a DOM event?
 
-  Your answer:
+  Your answer: A DOM event is anything that happens within the webpage such as loading, clicks, scroll overs, scaling window sizes and so on.
 
-  Researched answer:
+  Researched answer: What occurs when HTML is interacted with through user clicks on the UI. Javascript is executed (functions) when this happens.
 
 
 
 3. What is object-oriented programming? How is it different than functional programming?
 
-  Your answer:
+  Your answer: OOP is more focused around making real world objects connect through code such as the car project we worked on. Listing out each individual object to give them a purpose and then assigning different attributes to them in order to make a program run. It is generally a more lengthy style of coding, however, it can be executed just as quickly as functional programming. Functional programming is more oriented toward equation type programming that uses minimal lines of code in order to execute a function.
 
-  Researched answer:
+  Researched answer: Functional programming allows for easily managable and resusable code that is primarily used in front end spaces. OOP is more frequently used for back ends due to being able to give objects to the functional programming at the front.
 
 
 
 4. What is the difference between a Float and an Integer in Ruby?
 
-  Your answer:
+  Your answer: A float is simply a number that includes a decimal-not a whole number such as 7.3. An integer would simply be 7, 8, 9 and so on.
 
-  Researched answer:
+  Researched answer: I didn't find any further information for this.
 
 
 
 5. Ruby has an implicit return. What does that mean?
 
-  Your answer:
+  Your answer: I believe this means that you do not have to console log in order to get the code to run within your terminal.
 
-  Researched answer:
+  Researched answer: Ruby is capable of returning the last executed instruction in the code even without having it specifically called upon, which is called an implicit return.
 
 
 
 ## Looking Ahead: Terms for Next Week
 
-1. Instance Variable:
+1. Instance Variable: Used in OOP it is a variable that coincides with a class and has a separate copy of it
 
-2. PostgreSQL:
+2. PostgreSQL: Open source database that is object-relational. It uses SQL
 
-3. Ruby on Rails:
+3. Ruby on Rails: Provides a structure for web pages through the use of a server
 
-4. ORM:
+4. ORM: Object-relational mapping. Technique for converting data between incompatible systems by using OOP.
 
-5. Active Record:
+5. Active Record: An architectural pattern or anti-pattern. Stores in-memory object data in relational databases.
